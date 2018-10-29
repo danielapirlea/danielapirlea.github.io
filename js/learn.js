46e91be405f3b97501e8eb624116fbd8e57f8df4
danielapirlea <43964516+danielapirlea@users.noreply.github.com>
2018-10-29 20:26:37 +0200

    add string variables

--- js/learn.js
+++ js/learn.js
@@ -1,1 +1,19 @@
 console.info(0);
+
+var nume = "Pirlea";
+var prenume = "Daniela";
+var owner = "Daniela's";
+var msg1 = 'Ai zis "Dana"?';
+var msg2 = 'Nu, am zis "Daniela"';
+var template = "text cu ghilimele duble sau simple '";
+var age = "20";
+
+console.debug("auzi...? dar cum te cheama? -" + prenume);
+
+var skills = ["html", "css", "js"];
+
+prenume = "Dana";
+
+console.info("toate variabilele au fost initializate");
+console.log(nume);
+console.debug("cum te cheama? -" + prenume);
