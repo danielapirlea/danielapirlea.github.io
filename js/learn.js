4dd1e410755ab01911df15d7682215268a1607e0
danielapirlea <43964516+danielapirlea@users.noreply.github.com>
2018-10-29 20:58:29 +0200

    add curs valutar

--- js/learn.js
+++ js/learn.js
@@ -9,6 +9,7 @@ var template = "text cu ghilimele duble sau simple '";
 var age = "20";
 
 console.debug("auzi...? dar cum te cheama? -" + prenume);
+sayWelcome();
 
 var skills = ["html", "css", "js"];
 
@@ -17,3 +18,24 @@ prenume = "Dana";
 console.info("toate variabilele au fost initializate");
 console.log(nume);
 console.debug("cum te cheama? -" + prenume);
+sayWelcome();
+
+function sayWelcome() {
+  console.info("Salut " + prenume + ", bine ai venit la noi");
+}
+
+// schimb valutar
+
+var cursEUR = 4.5;
+var myEUR = 20;
+var myRON = 0;
+var friendEUR = 10;
+var friendRON = 0;
+
+// operatori * / + -
+
+myRON = myEUR * cursEUR;
+console.info("eu am " + myRON + " RON");
+
+friendRON = friendEUR * cursEUR;
+console.info("eu am " + friendRON + " RON");
