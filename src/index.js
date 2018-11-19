1272501aaa2050f5bf8ce899bc25ed380b75c135
Daniela Pîrlea <pirleadaniela@Pirleas-Air.localdomain>
2018-11-19 21:33:43 +0200

    add  function hideAllPages

--- src/index.js
+++ src/index.js
@@ -7,36 +7,58 @@ function hide(id) {
 function show(id) {
     $(id).style.display = "block";
 }
-function showSkillsPage() {
-    hide("home-page");
-    hide("education-page");
-    hide("languages-page");
-    show("skills-page");
+ // function showSkillsPage() {
+//     hide("home-page");
+//     hide("education-page");
+//     hide("languages-page");
+//     show("skills-page");
+// }
+ // function showEducationPage() {
+//     hide("home-page");
+//     hide("skills-page");
+//     hide("languages-page");
+//     show("education-page");
+// }
+
+// function showHomePage() {
+//     hide("skills-page");
+//     hide("education-page");
+//     hide("languages-page");
+//     show("home-page");
+// }
+// function showLanguagesPage() {
+//     hide("education-page");
+//     hide("home-page");
+//     hide("skills-page");
+//     show("languages-page");
+// }
+
+// $("home-menu").onclick = showHomePage;
+// $("skills-menu").onclick = showSkillsPage;
+// $("education-menu").onclick = showEducationPage;
+// $("languages-menu").onclick = showLanguagesPage;
+
+function hideAllPages() {
+    var pages = document.querySelectorAll(".page-block");
+    pages.forEach(function(page) {
+        // console.info("hide : " + page.id);
+        page.style.display = "none";
+    })
+//     for(var i=0; i < pages.length; i++) {
+//         pages[i].style.display = "none";
+//     }
 }
-function showEducationPage() {
-    hide("home-page");
-    hide("skills-page");
-    hide("languages-page");
-    show("education-page");
+var links = document.querySelectorAll("#top-menu-bar a");
+for (var i = 0; i < links.length; i++) {
+    // console.log(links[i].getAttribute("data-page"), links[i]);
+    links[i].onclick = function() {
+        console.warn("hide all pages");
+        hideAllPages();
+        var page = this.getAttribute("data-page")
+        // console.info("show page", page)
+        show(page + "-page");
+    };
 }
 
-function showHomePage() {
-    hide("skills-page");
-    hide("education-page");
-    hide("languages-page");
-    show("home-page");
-}
-function showLanguagesPage() {
-    hide("education-page");
-    hide("home-page");
-    hide("skills-page");
-    show("languages-page");
-}
-
-$("home-menu").onclick = showHomePage;
-$("skills-menu").onclick = showSkillsPage;
-$("education-menu").onclick = showEducationPage;
-$("languages-menu").onclick = showLanguagesPage;
-
-
+show("home-page");
 
