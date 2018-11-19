e6b11953a5a3f55c7ce6513ece5aa761341d52d1
Daniela Pîrlea <pirleadaniela@Pirleas-Air.localdomain>
2018-11-19 19:07:55 +0200

    show pages based on selected menu

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
-function showSkillsPage() {
-    document.getElementById("home-page").style.display = "none";
-    document.getElementById("skills-page").style.display = "block";
-    document.getElementById("languages-page").style.display = "none";
+function hide(id) {
+    document.getElementById(id).style.display = "none";
+}
+function show(id) {
+    document.getElementById(id).style.display = "block";
+
 }
 
-document.getElementById("skills-menu").onclick = showSkillsPage;
+function showSkillsPage() {
+   hide("home-page");
+   hide("languages-page");
+   show("skills-page");
+}
 
 function showHomePage() {
-    document.getElementById("home-page").style.display = "block";
-    document.getElementById("skills-page").style.display = "none";
-    document.getElementById("languages-page").style.display = "none";
+   hide("skills-page");
+   hide("languages-page");
+   shhow("home-page");
+}
+
+function showLanguagesPage() {
+   hide("home-page");
+   hide("skills-page");
+    show("languages-page");
 }
 
 document.getElementById("home-menu").onclick = showHomePage;
-
-function showLanguagesPage() {
-    document.getElementById("home-page").style.display = "none";
-    document.getElementById("skills-page").style.display = "none";
-    document.getElementById("languages-page").style.display = "block";
-}
-
+document.getElementById("skills-menu").onclick = showSkillsPage;
 document.getElementById("languages-menu").onclick = showLanguagesPage;
 
 
