61d2126e417bcbdfc8f6dde7d3cee986e2bcac72
Daniela Pîrlea <pirleadaniela@Pirleas-Air.localdomain>
2018-11-19 20:03:20 +0200

    add education page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function $(id) {
-   return document.getElementById(id);
+    return document.getElementById(id);
 }
 function hide(id) {
     $(id).style.display = "none";
@@ -7,27 +7,35 @@ function hide(id) {
 function show(id) {
     $(id).style.display = "block";
 }
-
 function showSkillsPage() {
-   hide("home-page");
-   hide("languages-page");
-   show("skills-page");
+    hide("home-page");
+    hide("education-page");
+    hide("languages-page");
+    show("skills-page");
+}
+function showEducationPage() {
+    hide("home-page");
+    hide("skills-page");
+    hide("languages-page");
+    show("education-page");
 }
 
 function showHomePage() {
-   hide("skills-page");
-   hide("languages-page");
-   show("home-page");
+    hide("skills-page");
+    hide("education-page");
+    hide("languages-page");
+    show("home-page");
 }
-
 function showLanguagesPage() {
-   hide("home-page");
-   hide("skills-page");
-   show("languages-page");
+    hide("education-page");
+    hide("home-page");
+    hide("skills-page");
+    show("languages-page");
 }
 
 $("home-menu").onclick = showHomePage;
 $("skills-menu").onclick = showSkillsPage;
+$("education-menu").onclick = showEducationPage;
 $("languages-menu").onclick = showLanguagesPage;
 
 
