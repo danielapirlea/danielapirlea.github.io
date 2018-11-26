8518c510048b19658a63d513b8f559601ec77668
Daniela Pîrlea <pirleadaniela@Pirleas-Air.localdomain>
2018-11-26 21:49:15 +0200

    generate skills from js

--- src/index.js
+++ src/index.js
@@ -7,58 +7,37 @@ function hide(id) {
 function show(id) {
     $(id).style.display = "block";
 }
- // function showSkillsPage() {
-//     hide("home-page");
-//     hide("education-page");
-//     hide("languages-page");
-//     show("skills-page");
-// }
- // function showEducationPage() {
-//     hide("home-page");
-//     hide("skills-page");
-//     hide("languages-page");
-//     show("education-page");
-// }
 
-// function showHomePage() {
-//     hide("skills-page");
-//     hide("education-page");
-//     hide("languages-page");
-//     show("home-page");
-// }
-// function showLanguagesPage() {
-//     hide("education-page");
-//     hide("home-page");
-//     hide("skills-page");
-//     show("languages-page");
-// }
-
-// $("home-menu").onclick = showHomePage;
-// $("skills-menu").onclick = showSkillsPage;
-// $("education-menu").onclick = showEducationPage;
-// $("languages-menu").onclick = showLanguagesPage;
+function hideElement(page) {
+    page.style.display = "none";
+}
 
 function hideAllPages() {
     var pages = document.querySelectorAll(".page-block");
-    pages.forEach(function(page) {
-        // console.info("hide : " + page.id);
-        page.style.display = "none";
-    })
-//     for(var i=0; i < pages.length; i++) {
-//         pages[i].style.display = "none";
-//     }
+    pages.forEach(hideElement);
 }
-var links = document.querySelectorAll("#top-menu-bar a");
-for (var i = 0; i < links.length; i++) {
-    // console.log(links[i].getAttribute("data-page"), links[i]);
-    links[i].onclick = function() {
-        console.warn("hide all pages");
-        hideAllPages();
-        var page = this.getAttribute("data-page")
-        // console.info("show page", page)
-        show(page + "-page");
-    };
+function initMenu() {
+    var links = document.querySelectorAll("#top-menu-bar a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            console.warn("hide all pages");
+            hideAllPages();
+            var page = this.getAttribute("data-page")
+            show(page + "-page");
+        };
+    }
 }
 
-show("home-page");
+function initSkillsPage() {
+    var skills = ['JS', 'HTML', ' CSS'];
+    var resultList = document.querySelector("#skills-page ul");
 
+var listItems = skills.map(function(skill) {
+    return `<li>${skill.toUpperCase()}</li>`;
+});
+    resultList.innerHTML = listItems.join('');
+}
+
+initMenu();
+show("skills-page");
+initSkillsPage();
